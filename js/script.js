a9732a82eac795d60cd7b2ebf00aa7b9ce6c6be4
Oliver Sibila <48774366+LastCapricorn@users.noreply.github.com>
2026-05-03 13:23:36 +0200

    Add GridResize and Drawing Functions
    - grid resize occurs in steps of 16 using two buttons
    - current resolution is displayed between these two buttons
    - LMB sets color (black), RMB erases color (white)
    - works with clicks and holding MBs
    - coloring is accompanied by a transition effect

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ const etchASketch = (() => {
 
   const sketchBoard = document.querySelector("#sketch-board");
   const gridToggler = document.querySelector("#grid-toggler");
+  const gridReset = document.querySelector("#grid-reset");
+  const lowerResolution = document.querySelector("#grid-minus");
+  const raiseResolution = document.querySelector("#grid-plus");
+  const penColors = ["black", "white"];
+  let gridResolution = 16;
   let gridSwitch = 1;
 
   function generateBoard(size) {
@@ -13,6 +18,7 @@ const etchASketch = (() => {
       for(let j = 0; j < size; j++) {
         const squareDiv = document.createElement("div");
         squareDiv.classList.add("square");
+        squareDiv.setAttribute("data-axis", [i, j]);
         divRow.appendChild(squareDiv);
       }
       sketchBoard.appendChild(divRow);
@@ -21,11 +27,51 @@ const etchASketch = (() => {
 
   function toggleGrid() {
     gridSwitch = gridSwitch === 0 ? 1 : 0;
-    const rows = document.querySelectorAll(".square-row");
-    for(const row of rows) () => row.style.setProperty("--grid-toggle", `${gridSwitch}px`);
     sketchBoard.style.setProperty("--grid-toggle", `${gridSwitch}px`);
+    sketchBoard.querySelectorAll(".square-row")
+      .forEach( row => row.style.setProperty("--grid-toggle", `${gridSwitch}px`) );
   }
 
-  generateBoard(16);
+  function resetGrid() {
+    sketchBoard.querySelectorAll(".square-row").forEach( row => row.remove() );
+    generateBoard(gridResolution);
+  }
+
+  generateBoard(gridResolution);
+
+  sketchBoard.addEventListener("contextmenu", ev => ev.preventDefault());
   gridToggler.addEventListener("click", toggleGrid);
+  gridReset.addEventListener("click", resetGrid);
+
+  lowerResolution.addEventListener("click", ev => {
+    if (gridResolution === 16) return;
+    gridResolution -= 16;
+    document.querySelector("#grid-size").textContent = `${gridResolution} x ${gridResolution}`;
+    resetGrid();
+  });
+  raiseResolution.addEventListener("click", ev => {
+    if (gridResolution === 96) return;
+    gridResolution += 16;
+    document.querySelector("#grid-size").textContent = `${gridResolution} x ${gridResolution}`;
+    resetGrid();
+  });
+
+  sketchBoard.addEventListener("mouseenter", ev => {
+    if (!ev.target.dataset.axis || ev.buttons === 0) return;
+    ev.target.classList.add("current");
+    if (ev.buttons === 1 || ev.buttons === 2) {
+      ev.target.style.backgroundColor = penColors[ev.buttons - 1];
+    } 
+  }, 1);
+  sketchBoard.addEventListener("mousedown", ev => {
+    if (!ev.target.dataset.axis || ev.buttons === 0) return;
+    if (ev.buttons === 1 || ev.buttons === 2) {
+      ev.target.style.backgroundColor = penColors[ev.buttons - 1];
+    } 
+  });
+  sketchBoard.addEventListener("mouseleave", ev => {
+    if (!ev.target.dataset.axis || ev.buttons === 0) return;
+    ev.target.classList.remove("current");
+  }, 1);
+
 })();
